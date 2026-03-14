63c1e0794425bf329865f8dfa81a10f4a20283a5
Rutunjay Sonune <rutunjaysonune309@gmail.com>
2026-03-14 21:55:30 +0530

    Update server.js

--- cineby-stremio-addon/server.js
+++ cineby-stremio-addon/server.js
@@ -1,1 +1,39 @@
+const { addonBuilder, serveHTTP } = require("stremio-addon-sdk")
 
+const manifest = {
+    id: "org.cineby.vidking",
+    version: "1.0.0",
+    name: "Cineby",
+    description: "Watch movies and series from Cineby",
+    resources: ["stream"],
+    types: ["movie", "series"],
+    idPrefixes: ["tt"]
+}
+
+const builder = new addonBuilder(manifest)
+
+builder.defineStreamHandler(({ type, id }) => {
+
+    const imdb = id.split(":")[0]
+
+    let url
+
+    if (type === "movie") {
+        url = "https://www.vidking.net/embed/movie/1078605"
+    } else {
+        url = "https://www.vidking.net/embed/tv/119051/1/1"
+    }
+
+    return Promise.resolve({
+        streams: [
+            {
+                title: "Cineby (Vidking)",
+                url: url
+            }
+        ]
+    })
+})
+
+const addonInterface = builder.getInterface()
+
+serveHTTP(addonInterface, { port: 7000 })
