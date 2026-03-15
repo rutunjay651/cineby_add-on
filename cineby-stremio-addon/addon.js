33da204ccc0712c268706ca314d8cfba0220aa87
Rutunjay Sonune <rutunjaysonune309@gmail.com>
2026-03-15 13:56:46 +0530

    Update addon.js

--- cineby-stremio-addon/addon.js
+++ cineby-stremio-addon/addon.js
@@ -6,15 +6,8 @@ const TMDB_API_KEY = "e4598ac9cb6d28883dac12852c670c5a"
 
 const builder = new addonBuilder(manifest)
 
-function imdbToTmdb(imdbId) {
-    return new Promise((resolve) => {
-
-        const url =
-            "https://api.themoviedb.org/3/find/" +
-            imdbId +
-            "?api_key=" +
-            TMDB_API_KEY +
-            "&external_source=imdb_id"
+function fetch(url) {
+    return new Promise((resolve, reject) => {
 
         https.get(url, (res) => {
 
@@ -22,27 +15,44 @@ function imdbToTmdb(imdbId) {
 
             res.on("data", chunk => data += chunk)
 
-            res.on("end", () => {
+            res.on("end", () => resolve(data))
 
-                try {
-
-                    const json = JSON.parse(data)
-
-                    if (json.movie_results && json.movie_results.length)
-                        return resolve({ type: "movie", id: json.movie_results[0].id })
-
-                    if (json.tv_results && json.tv_results.length)
-                        return resolve({ type: "tv", id: json.tv_results[0].id })
-
-                } catch {}
-
-                resolve(null)
-
-            })
-
-        }).on("error", () => resolve(null))
+        }).on("error", reject)
 
     })
+}
+
+async function imdbToTmdb(imdbId) {
+
+    const url =
+        "https://api.themoviedb.org/3/find/" +
+        imdbId +
+        "?api_key=" +
+        TMDB_API_KEY +
+        "&external_source=imdb_id"
+
+    const data = await fetch(url)
+    const json = JSON.parse(data)
+
+    if (json.movie_results && json.movie_results.length)
+        return { type: "movie", id: json.movie_results[0].id }
+
+    if (json.tv_results && json.tv_results.length)
+        return { type: "tv", id: json.tv_results[0].id }
+
+    return null
+}
+
+async function extractStream(embedUrl) {
+
+    const html = await fetch(embedUrl)
+
+    const match = html.match(/https?:\/\/[^"]+\.m3u8[^"]*/)
+
+    if (match)
+        return match[0]
+
+    return null
 }
 
 builder.defineStreamHandler(async ({ type, id }) => {
@@ -54,32 +64,37 @@ builder.defineStreamHandler(async ({ type, id }) => {
     if (!tmdb)
         return { streams: [] }
 
-    let streamUrl
+    let embedUrl
 
     if (tmdb.type === "movie") {
 
-        streamUrl = "https://www.vidking.net/embed/movie/" + tmdb.id
+        embedUrl = "https://www.vidking.net/embed/movie/" + tmdb.id
 
     } else {
 
         const season = id.split(":")[1]
         const episode = id.split(":")[2]
 
-        streamUrl =
+        embedUrl =
             "https://www.vidking.net/embed/tv/" +
             tmdb.id +
             "/" +
             season +
             "/" +
             episode
-
     }
+
+    const streamUrl = await extractStream(embedUrl)
+
+    if (!streamUrl)
+        return { streams: [] }
 
     return {
         streams: [
             {
-                title: "Cineby (Vidking)",
-                url: streamUrl
+                title: "Cineby 1080p",
+                url: streamUrl,
+                type: "hls"
             }
         ]
     }
