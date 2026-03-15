36de0d8c24c8c4a79c18a72660aa531cba48c9d6
Rutunjay Sonune <rutunjaysonune309@gmail.com>
2026-03-15 13:04:56 +0530

    Update addon.js

--- cineby-stremio-addon/addon.js
+++ cineby-stremio-addon/addon.js
@@ -1,26 +1,89 @@
 const { addonBuilder } = require("stremio-addon-sdk")
+const https = require("https")
 const manifest = require("./manifest.json")
+
+const TMDB_API_KEY = "UJL9E8CG8JNUEKJNJD7BEBNNJGPPVGEL"
 
 const builder = new addonBuilder(manifest)
 
-builder.defineStreamHandler(({ type }) => {
+function imdbToTmdb(imdbId) {
+    return new Promise((resolve) => {
 
-    let url
+        const url =
+            "https://api.themoviedb.org/3/find/" +
+            imdbId +
+            "?api_key=" +
+            TMDB_API_KEY +
+            "&external_source=imdb_id"
 
-    if (type === "movie") {
-        url = "https://www.vidking.net/embed/movie/1078605"
+        https.get(url, (res) => {
+
+            let data = ""
+
+            res.on("data", chunk => data += chunk)
+
+            res.on("end", () => {
+
+                try {
+
+                    const json = JSON.parse(data)
+
+                    if (json.movie_results && json.movie_results.length)
+                        return resolve({ type: "movie", id: json.movie_results[0].id })
+
+                    if (json.tv_results && json.tv_results.length)
+                        return resolve({ type: "tv", id: json.tv_results[0].id })
+
+                } catch {}
+
+                resolve(null)
+
+            })
+
+        }).on("error", () => resolve(null))
+
+    })
+}
+
+builder.defineStreamHandler(async ({ type, id }) => {
+
+    const imdbId = id.split(":")[0]
+
+    const tmdb = await imdbToTmdb(imdbId)
+
+    if (!tmdb)
+        return { streams: [] }
+
+    let streamUrl
+
+    if (tmdb.type === "movie") {
+
+        streamUrl = "https://www.vidking.net/embed/movie/" + tmdb.id
+
     } else {
-        url = "https://www.vidking.net/embed/tv/1399/1/1"
+
+        const season = id.split(":")[1]
+        const episode = id.split(":")[2]
+
+        streamUrl =
+            "https://www.vidking.net/embed/tv/" +
+            tmdb.id +
+            "/" +
+            season +
+            "/" +
+            episode
+
     }
 
-    return Promise.resolve({
+    return {
         streams: [
             {
                 title: "Cineby (Vidking)",
-                url: url
+                url: streamUrl
             }
         ]
-    })
+    }
+
 })
 
 module.exports = builder.getInterface()
